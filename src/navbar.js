617327d6b8c1d07ba124f1198c4847bf71cbefa2
Indra Meyers <67623845+DarK-Zer0@users.noreply.github.com>
2024-03-02 19:10:49 -0500

    Navigation Bar Finalization

---
+++ src/navbar.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    var dropdownMenu = document.getElementById("dropdownMenu");
+    if (dropdownMenu.style.display === "none") {
+      dropdownMenu.style.display = "block";
+    } else {
+      dropdownMenu.style.display = "none";
+    }
+}
