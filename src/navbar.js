4d20d9bed6637672ed46c79dab67fcb5d2132826
Indra Meyers <67623845+DarK-Zer0@users.noreply.github.com>
2024-03-14 14:05:58 -0400

    Nav Bar Styling Adjustments

--- src/navbar.js
+++ src/navbar.js
@@ -6,3 +6,13 @@ function toggleDropdown() {
       dropdownMenu.style.display = "none";
     }
 }
+
+function checkWindowSize() {
+  const dropdownMenu = document.getElementById('dropdownMenu');
+  if (window.innerWidth > 1240) {
+      dropdownMenu.style.display = 'none';
+  }
+}
+
+// Call checkWindowSize function on window resize
+window.addEventListener('resize', checkWindowSize);
